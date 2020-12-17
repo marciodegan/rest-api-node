205c1e4ea33a1d8aba171026eb9f7aadb12327d3
marciodegan <marciodegan@yahoo.com>
2020-12-17 12:31:03 -0300

    initial routes

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+app.use(morgan('dev'));
+
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+
+// WHEN ROUTE NOT FOUND
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+});
+
+module.exports = app;
+
+// after creating, import it to server.js
